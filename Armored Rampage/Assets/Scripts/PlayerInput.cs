8043b79c73fb1a02ab19422657660281255217f2
Csaba Vas <48047148+CsabaVas44@users.noreply.github.com>
2023-03-10 20:25:01 +0100

    Create PlayerInput.cs

---
+++ Armored Rampage/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    void Update()
+    {
+        GetBodyMovement();
+        GetTurretMovement();
+        GetShootingInput();
+    }
+
+    private void GetTurretMovement()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void GetShootingInput()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void GetBodyMovement()
+    {
+        throw new NotImplementedException();
+    }
+}
