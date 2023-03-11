6f00408615e13764b12dc261502c9a74e9e68c4f
Csaba Vas <48047148+CsabaVas44@users.noreply.github.com>
2023-03-11 17:24:06 +0100

    ADD – Shooting and AI Player detection

---
+++ Armored Rampage/Assets/Scripts/TankMover.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMover : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float maxSpeed = 10;
+    public float rotationSpeed = 100;
+    Vector2 movementVector;
+   
+
+    private void Awake()
+    {
+        rb2d = GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = transform.up * movementVector.y * maxSpeed * Time.fixedDeltaTime;
+        rb2d.MoveRotation(transform.rotation * Quaternion.Euler(0, 0, -movementVector.x * rotationSpeed * Time.fixedDeltaTime));
+
+    }
+
+    public void Move(Vector2 movementVector)
+    {
+        this.movementVector = movementVector;
+    }
+}
