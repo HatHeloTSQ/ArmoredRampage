a460f8ea3372091c7799d02170d88139d4640466
Karikó-Tóth Máté <karikototh.mate02@gmail.com>
2023-03-11 23:07:36 +0100

    Options menu script created
    Added functionality:
    - set graphics quality
    - switch to fullscreen
    - todo: resolution scaling

---
+++ Armored Rampage/Assets/Scenes/MainMenu/OptionsMenuScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenuScript : MonoBehaviour
+{
+    private Resolution[] resolutions;
+    public Dropdown resDropdown;
+
+    //TODO fix resolution menu
+
+    /*private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resDropdown.ClearOptions();
+
+        var resOptions = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            resOptions.Add(resolutions[i].width + " x " + resolutions[i].height);
+        }
+        resDropdown.AddOptions(resOptions);
+    }*/
+
+    public void GraphQualityDropdown_SetQuality(int qualityIdx)
+    {
+        QualitySettings.SetQualityLevel(qualityIdx); 
+    }
+
+    public void FullscreenToggle_SetFullscreen(bool isFullscreen)
+    {
+        Debug.Log($"Fullscreen value is {isFullscreen}");
+        Screen.fullScreen = isFullscreen;
+    }
+}
