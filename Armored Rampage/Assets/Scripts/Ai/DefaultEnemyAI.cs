5f28e4ed1a0fd5865e78821cc6641014ba48c390
Csaba Vas <48047148+CsabaVas44@users.noreply.github.com>
2023-03-11 22:25:10 +0100

    ADD STATIC AI SHOOTING AND PATROL

---
+++ Armored Rampage/Assets/Scripts/Ai/DefaultEnemyAI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultEnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    private AIBehaviour shootBehaviour, patrolBehaviour;
+
+    [SerializeField]
+    TankController tank;
+
+    [SerializeField]
+    AiDetector detector;
+
+    private void Awake()
+    {
+        detector = GetComponentInChildren<AiDetector>();
+        tank = GetComponentInChildren<TankController>();
+    }
+
+    private void Update()
+    {
+        if (detector.TargetVisible)
+        {
+            shootBehaviour.PerformAction(tank, detector);
+        }
+        else
+        {
+            patrolBehaviour.PerformAction(tank, detector);
+        }
+    }
+
+
+}
