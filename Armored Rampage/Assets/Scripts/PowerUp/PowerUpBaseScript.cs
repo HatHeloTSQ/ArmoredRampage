1a1050a089c364f90516d21b1c2031887428131c
Karikó-Tóth Máté <karikototh.mate02@gmail.com>
2023-03-23 00:12:51 +0100

    Completely reworked powerup system

--- Armored Rampage/Assets/Scripts/PowerUp/PowerUpBaseScript.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PowerUpBaseScript : ScriptableObject
-{
-    public abstract void ApplyPowerUp(GameObject gameObject);
-}
