cacd523b7940808b3d6bd2438e35405c6a7fd501
Karikó-Tóth Máté <karikototh.mate02@gmail.com>
2023-03-23 00:13:47 +0100

    Added max hp buff
    Gives you more hp for a duration
    - might add: restore hp along with the max hp buff

---
+++ Armored Rampage/Assets/Scripts/PowerUp/MaxHPBuff.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxHPBuff : MonoBehaviour
+{
+    public int buffValue;
+    public float duration;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) StartCoroutine(ApplyPowerUp(collision));
+    }
+
+    private IEnumerator ApplyPowerUp(Collider2D collisionObj)
+    {
+        collisionObj.GetComponentInChildren<Damagable>().MaxHealth += buffValue;
+
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        collisionObj.GetComponentInChildren<TankMover>().maxSpeed -= buffValue;
+        Destroy(this.gameObject);
+
+    }
+
+}
