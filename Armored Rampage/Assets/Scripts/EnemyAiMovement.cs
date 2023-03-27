78fe0f6b1605c8445008b33bccb73f46184eb011
Pocsai Norbert <pocsainorbertjanos@gmail.com>
2023-03-27 17:03:12 +0200

    Patrolling tank static sprites done

---
+++ Armored Rampage/Assets/Scripts/EnemyAiMovement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts
+{
+    class EnemyAiMovement : MonoBehaviour
+    {
+        NavMeshAgent agent;
+        public Transform[] waypoints;
+        int waypointIndex;
+        Vector3 target;
+
+        private void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (Vector3.Distance(transform.position, target) < 1)
+            {
+                IterateWaypointIndex();
+                UpdateDestination();
+            }
+        }
+
+        void UpdateDestination()
+        {
+            target = waypoints[waypointIndex].position;
+            agent.SetDestination(target);
+
+        }
+        void IterateWaypointIndex()
+        {
+            waypointIndex++;
+            if (waypointIndex == waypoints.Length)
+            {
+                waypointIndex = 0;
+            }
+        }
+    }
+
+
+}
--- Armored Rampage/Assets/Scripts/RandomMovement.cs
+++ Armored Rampage/Assets/Scripts/RandomMovement.cs
@@ -19,27 +19,45 @@ public class RandomMovement : MonoBehaviour
     public List<Sprite> seSprites;
     public List<Sprite> sSprites;
     Vector2 direction;
+    Vector2 transformDir;
+    int counter;
     public SpriteRenderer spriteRenderer;
     void Start()
     {
         waitTime = startWaitTime;
         randomSpot = Random.Range(0, moveSpots.Length);
         spriteRenderer = GetComponent<SpriteRenderer>();
+        direction = transform.position;
     }
 
-
-    Vector2 moveDir;
-    void FixedUpdate()
+    void Update()
     {
-        
-
 
         transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
-
+        
+        
+        if (Time.realtimeSinceStartup % 1 > 0 && Time.realtimeSinceStartup % 1 < 0.3)
+        {
+            direction = transform.position;
+        }
+        if (Time.realtimeSinceStartup % 1 >= 0.3)
+        {
+            transformDir = transform.position;
+        }
 
 
         HandleSpriteFlip();
+        
+        List<Sprite> directionSprites = GetSpriteDirection();
 
+        if (directionSprites != null)
+        {
+            spriteRenderer.sprite = directionSprites[0];
+        }
+        else
+        {
+
+        }
 
 
         if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
@@ -60,19 +78,45 @@ public class RandomMovement : MonoBehaviour
 
     private void HandleSpriteFlip()
     {
-        if (!spriteRenderer.flipX && transform.position.x > 0)
+        if (!spriteRenderer.flipX && direction.x > transform.position.x)
         {
             spriteRenderer.flipX = true;
             Debug.Log("Flipped left");
         }
-        else if (spriteRenderer.flipX && transform.position.x < 0)
+        else if (spriteRenderer.flipX && direction.x < transform.position.x)
         {            
             spriteRenderer.flipX = false;
             Debug.Log("Flipped right");
         }
     }
+    List<Sprite> GetSpriteDirection()
+    {
+        List<Sprite> selectedSprites = null;
 
-    
+        if (transformDir.x > direction.x)
+        {
+            if (transformDir.y > direction.y)
+            {
+                selectedSprites = neSprites;
+            }
+            else if (transformDir.y < direction.y)
+            {
+                selectedSprites = seSprites;
+            }
+            else
+            {
+                selectedSprites = eSprites;
+            }
+        }
+        if (transformDir.x == direction.x)
+        {
+            selectedSprites = nSprites;
+        }
+
+
+        return selectedSprites;
+    }
+
 
 
 }
